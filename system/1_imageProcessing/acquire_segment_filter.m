1150c1c40d911ba0c12de94d6131bae5be3caf3e
Andre Perez <andre_mperez@hotmail.com>
2017-11-07 18:12:55 -0200

    Update acquire_segment_filter.m

--- system/1_imageProcessing/acquire_segment_filter.m
+++ system/1_imageProcessing/acquire_segment_filter.m
@@ -1,15 +1,38 @@
+%**************************************************************************
+% @INSTITUTION
+%  University of Sao Paulo | Sao Carlos School of Engineering | SEL
 %--------------------------------------------------------------------------
-% Project:      Automation of Zika Virus plaque counting from the plaque 
-%               assay experiment using computer vision
+% @DISCIPLINE
+%  Name: SEL0444 | Final Year Project
+%  Professor: Marcelo Andrade da Costa Vieira
+%  Semester: 2017\02
 %--------------------------------------------------------------------------
-% Step:         1.  - IMAGE PROCESSING
-% Block:        1.1 - Well image acquisition
-%               1.2 - Segmentation 
-%               1.3 - Filtering
+% @DEVELOPMENT
+%  IDE: MATLAB R2013a
 %--------------------------------------------------------------------------
-% Author:       Andre Marcos Perez
-% Contact:      andre.marcos.perez@usp.br
+% @WARRANTY
+%  Copyright (c) 2017 Andre Marcos Perez
+%  The software is provided by "as is", without warranty of any kind, 
+%  express or implied, including but not limited to the warranties of 
+%  merchantability, fitness for a particular purpose and noninfringement. 
+%  In no event shall the authors or copyright holders be liable for any 
+%  claim, damages or other liability, whether in an action of contract, 
+%  tort or otherwise, arising from, out of or in connection with the 
+%  software or the use or other dealings in the software.
 %--------------------------------------------------------------------------
+% @AUTHOR
+%  Name:  Andre Marcos Perez
+%  Email: andre.marcos.perez@usp.br
+%  #USP:  8006891
+%--------------------------------------------------------------------------
+% @DESCRIPTION
+%  Title: Automation of Zika Virus plaque counting from the plaque 
+%         assay experiment using computer vision
+%  Step:  1.  - IMAGE PROCESSING
+%  Block: 1.1 - Well image acquisition
+%         1.2 - Segmentation 
+%         1.3 - Filtering
+%**************************************************************************
 
 %--------------------------------------------------------------------------
 % Prepare environment
